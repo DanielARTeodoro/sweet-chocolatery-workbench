ef9c8957d92e496c973c21cdbc948d9d1004f311
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-11-11 00:40:31 -0300

    Update SweetChocolateryWorkbenchApplication.java
    Uma Atualização boba da index do arquivo index.html.

--- Soma/src/main/java/br/edu/ifms/workbenchapp/SweetChocolateryWorkbenchApplication.java
+++ Soma/src/main/java/br/edu/ifms/workbenchapp/SweetChocolateryWorkbenchApplication.java
@@ -2,12 +2,22 @@ package br.edu.ifms.workbenchapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class SweetChocolateryWorkbenchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SweetChocolateryWorkbenchApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
