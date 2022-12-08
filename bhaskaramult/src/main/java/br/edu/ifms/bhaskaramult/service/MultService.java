2b8fb6450ed65736c3bf3d7961734ac0af3afad9
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-12-08 04:06:41 -0300

    Mult-ExplosãoFogueteDeCoelhinhosDeMarshmallow(6.0)
    Mult - Explosão Foguete De Coelhinhos De Marshmallow (6.0)
    
    Update do Serviço da Multiplicação.

--- bhaskaramult/src/main/java/br/edu/ifms/bhaskaramult/service/MultService.java
+++ bhaskaramult/src/main/java/br/edu/ifms/bhaskaramult/service/MultService.java
@@ -1,12 +1,17 @@
 package br.edu.ifms.bhaskaramult.service;
 
+import br.edu.ifms.bhaskaramult.exception.NotNumericException;
 import br.edu.ifms.bhaskaramult.model.Mult;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MultService {
-    public Mult create(Mult multing) {
-        Integer s = multing.getX() * multing.getY();
-        return multing;
+    public Integer multiplicar(Mult multing) {
+        try {
+            return multing.getX() * multing.getY();
+        } catch (Exception exception) {
+            throw new NotNumericException("Não é permitido letras na conta!");
+        }
+
     }
 }
