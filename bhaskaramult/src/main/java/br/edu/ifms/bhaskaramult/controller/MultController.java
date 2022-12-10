a3f35bdfab1a2e27fc7abdbc08769d58cb3bab69
Dani <51429507+DanielARTeodoro@users.noreply.github.com>
2022-12-09 21:22:13 -0300

    Mult-MgaExplosõDestruidorDCogumelNougat(7.1)
    Mult-MgaExplosõDestruidorDCogumelNougat(7.1)
    
    Update - Mult.

--- bhaskaramult/src/main/java/br/edu/ifms/bhaskaramult/controller/MultController.java
+++ bhaskaramult/src/main/java/br/edu/ifms/bhaskaramult/controller/MultController.java
@@ -19,10 +19,14 @@ public class MultController {
 
     /// Método GET
     @GetMapping(value = "/{x}/{y}")
-    public Integer getMultPathParam(@PathVariable String x, @PathVariable String y) {
-        if (x.matches("[0-9]+") && y.matches("[0-9]+")) {
-            return Integer.parseInt(x) * Integer.parseInt(y);
-        } else {
+    public Double getMultPathParam(@PathVariable String x, @PathVariable String y) {
+        ///if (x.matches("[0-9].") && y.matches("[0-9].")) {
+        try {
+            Double num1 = Double.parseDouble(x);
+            Double num2 = Double.parseDouble(y);
+            return num1 * num2;
+        } catch (Exception e) {
+            //} else {
             throw new NotNumericException("Não é permitido letras na conta!");
         }
     }
@@ -30,7 +34,7 @@ public class MultController {
     /// Método POST
     @PostMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public Integer multPost(@RequestBody Mult multing) {
+    public Double multPost(@RequestBody Mult multing) {
         log.info("Multiplicação da Conta só pode ter números - Erro: 502", HttpStatus.BAD_GATEWAY.toString());
         log.info("Multiplicação da Conta só pode ter números - Erro: 400", HttpStatus.BAD_REQUEST.toString());
         log.info("Multiplicação da Conta não aceitar tal requisição - Erro: 510", HttpStatus.NOT_EXTENDED.toString());
